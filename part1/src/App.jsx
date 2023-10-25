b1faa12decd1af2b89469c11b050b0c56e6f6306
klau55 <kipatok@bk.ru>
2023-10-25 22:23:05 +0300

    123

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,22 @@
-const App = () => {
+const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
