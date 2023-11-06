1e55ce8a12181a6aa8da1834253a0779dcb2a675
klau55 <kipatok@bk.ru>
2023-11-07 00:30:21 +0200

    2.10 done, onto part 2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
+import Persons from './Persons'
+import PersonForm from './PersonForm'
+import Filter from './Filter'
 
-const Persons = ({ personsToShow, filter }) => {
-
-    return (
-      personsToShow.map(person =>
-        <p key={person.id}>{person.name} {person.number}</p>
-      )
-    )
-  }
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -22,16 +17,13 @@ const App = () => {
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter))
 
-  const handleNameChange = (event) => {
+  const handleNameChange = (event) => 
     setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
+  const handleNumberChange = (event) => 
     setNewNumber(event.target.value)
-  }
-  const handleFilterChange = (event) => {
+  const handleFilterChange = (event) => 
     setFilter(event.target.value)
-
-  }
+  
   const addName = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName))
@@ -40,35 +32,25 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
-    else {
-    const nameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    }
-    
-    
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
-  }}
+    else 
+    {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }}
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input onChange={handleFilterChange} /></div>
-      <form onSubmit={addName}>
-        <div>
-          name:  <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm newName={newName} newNumber={newNumber} addName={addName} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} filter={filter} />
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
---
+++ part2/phonebook/src/Filter.jsx
@@ -0,0 +1,6 @@
+const Filter = ({ handleFilterChange }) => {
+    return (
+      <div>filter shown with <input onChange={handleFilterChange} /></div>
+    )
+  }
+export default Filter
