d4cc46795013012d1b5d09cffac09e977bcbed28
klau55 <kipatok@bk.ru>
2023-11-23 01:11:14 +0200

    3.7 & 3.8 done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ﻿# Full-Stack-Open
  Completing the course from University of Helsinki on Full Stack Web Development
 https://fullstackopen.com/
+
+    "start": "node index.js",
+    "dev": "nodemon index.js",
--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+morgan.token('res-body', (request) => JSON.stringify(request.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-body'))
 
 let persons = 
 [
@@ -70,7 +73,6 @@ const generateId = () => {
     }
   
     persons = persons.concat(person)
-  
     response.json(person)
   })
 
