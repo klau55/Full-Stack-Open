46b1dba3dd2f058879068f35c1573acc8f165255
klau55 <kipatok@bk.ru>
2023-11-26 19:57:49 +0200

    wip

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 morgan.token('res-body', (request) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-body'))
