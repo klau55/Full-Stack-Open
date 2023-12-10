e17205e0483b6e8857c0f948e895e054d52c518c
klau55 <kipatok@bk.ru>
2023-12-10 19:56:53 +0200

    3.17 & 3.18

--- part3/index.js
+++ part3/index.js
@@ -22,12 +22,11 @@ const generateId = () => {
 
   app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-  
       response.json(persons)
     })
   })
 
-  app.post('/api/persons', (req, res) => {
+  app.post('/api/persons', (req, res, next) => {
     const body = req.body
     if (!body) {
       return res.status(400).json({ 
@@ -43,22 +42,39 @@ const generateId = () => {
     newPerson.save().then(savedPerson => {
     res.json(savedPerson)
     })
+    .catch(error => next(error))
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(() => {
       response.status(204).end()
     })
-//    .catch
-//      response.status(404).end()
+    .catch(error => next(error))
+      
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person)
+    } else {
+      response.status(404).json({ error: 'contact not found' })
+    }
     })
-  })
+    .catch(error => next(error))
+    })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+  }
+  app.use(errorHandler)
   
  const PORT = process.env.PORT
   app.listen(PORT, () => {
