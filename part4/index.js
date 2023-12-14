613cd14e61df8f6201b3bc516161cf74d698ba5a
klau55 <kipatok@bk.ru>
2023-12-14 22:43:34 +0200

    4.1

--- part4/index.js
+++ part4/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -12,7 +13,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://teperyazadrot:nkUjJYRXYSEwPlaT@cluster0.5isbinn.mongodb.net/?retryWrites=true&w=majority'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
