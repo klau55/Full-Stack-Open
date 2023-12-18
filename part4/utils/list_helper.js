70aea2dc3042a0703c42b063bd3b510111f187f1
klau55 <kipatok@bk.ru>
2023-12-18 21:59:38 +0200

    4.4 & 4.5 done. tests setting up

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((n, blog) => Number(n) + Number(blog.likes), 0)
+  }
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((res, blog) => Number(res.likes) < Number(blog.likes)?
+   blog.likes : res, blogs[0])
+}
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog
   }
