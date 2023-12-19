5eee5c8111e598f1d79fced9f23470645edd3974
klau55 <kipatok@bk.ru>
2023-12-19 23:06:06 +0200

    4.8

--- part4/tests/test.js
+++ part4/tests/test.js
@@ -1,4 +1,4 @@
-const listHelper = require('../list_helper')
+const listHelper = require('../utils/list_helper')
 const listWithOneBlog = [
   {
     _id: '5a422aa71b54a676234d17f8',
--- part4/utils/config.js
+++ part4/utils/config.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const PORT = 3003
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
     PORT,
