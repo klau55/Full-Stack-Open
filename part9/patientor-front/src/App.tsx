74c190f51d9772f1e8fdc1f80bbe513838aa56c9
klau55 <kipatok@bk.ru>
2024-03-18 17:40:03 +0200

    9.12 - 9.13

--- part9/patientor-front/src/App.tsx
+++ part9/patientor-front/src/App.tsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
-import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
@@ -13,11 +11,9 @@ const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
 
   useEffect(() => {
-    void axios.get<void>(`${apiBaseUrl}/`);
 
     const fetchPatientList = async () => {
       const patients = await patientService.getAll();
-      console.log('patients', patients);
       setPatients(patients);
     };
     void fetchPatientList();
